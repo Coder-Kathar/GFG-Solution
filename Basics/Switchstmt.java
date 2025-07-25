782459537e9498bacd86eddc84613d1e88c22ece
unknown <katharbasha1407@gmail.com>
2025-07-25 13:52:45 +0530

    Java Switch case Statement Problem Solution

---
+++ Basics/Switchstmt.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Switchstmt {
+    // User function Template for Java
+    static double switchCase(int choice, List<Double> arr) {
+        // code here
+        double ans = 0.0;
+        switch(choice)
+        {
+            case 1:
+                ans = Math.PI * arr.get(0) * arr.get(0);
+                break;
+            case 2:
+                ans = arr.get(0) * arr.get(1);
+                break;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int choice = sc.nextInt();
+        String s = sc.nextLine();
+        String[] str_arr = s.split("\\s+");
+        List<Double> arr = new ArrayList<>();
+        for(String x : str_arr)
+        {
+            double val = (double) Integer.parseInt(x);
+            arr.add(val);
+        }
+        System.out.println(switchCase(choice, arr));
+    }
+}
+
